f8e4d4f7b2e6e864df70a08250c683d73024fefb
tuan-nguye <70942348+tuan-nguye@users.noreply.github.com>
2022-10-29 17:38:11 +0200

    random movement when inactive

--- project/enemy/Enemy.gd
+++ project/enemy/Enemy.gd
@@ -19,11 +19,19 @@ var player
 # maps the function name of the attack to the wait time (attack speed)
 var attack_map
 
+# move in random direction when inactive
+var random_vector
+
+# move random for i iterations
+var random_i
+
 func _init(speed = 5, sight = 300, hp = 100):
 	self.speed = speed
 	self.sight = sight
 	self.hp = hp
 	attack_map = {}
+	random_vector = Vector2()
+	random_i = 0
 	
 
 # Called when the node enters the scene tree for the first time.
@@ -32,11 +40,28 @@ func _ready():
 	player = get_node("/root/Game/Player")
 	register_attacks()
 
-# get player position and move towards him
 func move():
+	if(in_sight()):
+		move_towards_player()
+	else:
+		move_random()
+
+# get player position and move towards him
+func move_towards_player():
 	var velocity_vector = get_delta_vector()
 	velocity_vector = velocity_vector.normalized() * speed;
 	set_position(get_position() + velocity_vector)
+
+func move_random():
+	if(random_i > 0):
+		set_position(get_position() + random_vector)
+		random_i -= 1
+	else:
+		random_vector.x = rand_range(-1, 1)
+		random_vector.y = rand_range(-1, 1)
+		random_vector = random_vector.normalized() * 0.2
+		random_i = rand_range(0, 500)
+	
 
 # check if player is in sight
 # pythagoras: root of (x²+y²) = radius
@@ -73,8 +98,7 @@ func add_attack(attack_name, wait_time):
 	attack_map[attack_name] = wait_time
 
 func _physics_process(delta):
-	if(in_sight()):
-		move()
+	move()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
