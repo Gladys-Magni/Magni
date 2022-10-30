8ca0a49634c845af15619f584f4c648489645fe5
timhanel <tim.hanel@mailbox.tu-dresden.de>
2022-10-30 19:02:39 +0100

    Add cannon balls logic

--- project/player/Kapitaen.gd
+++ project/player/Kapitaen.gd
@@ -1,14 +1,13 @@
 extends KinematicBody2D
 
-const ACCELERATION = 1
 const MAX_SPEED = 200
 const FRICTION = 1
 var inShop = false
 var stats = {"Health": 1, "CannonBalls": 1, "Coins": 100, "DamageMultiplicator": 1, "BoatSpeed": 1, "Range": 1, "ReloadTime": 5}
 var level = 1
 var health = 1
-
 const HeartSize = 15
+const CannonSize = 2480
 
 var velocity = Vector2.ZERO
 
@@ -19,8 +18,11 @@ onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var mouseIcon = get_node("/root/Game/MouseIcon")
 onready var timer=$timer
+onready var reloadTimer = $ReloadTimer
 onready var cannonCollection=$cannons
 var loaded=true
+var reloaded = true
+var cannonBallsLeft = 5
 
 func _set_up_cannon_ball():
 	var dir = self.global_position.direction_to(get_global_mouse_position())
@@ -49,19 +51,25 @@ func _physics_process(delta):
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		
 		CurrentDirection=input_vector
-		velocity += input_vector * ACCELERATION * delta
+		velocity += input_vector * self.stats["BoatSpeed"] * delta
 		velocity = velocity.clamped(MAX_SPEED * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	move_and_collide(velocity)
 
-	if Input.is_action_just_pressed("shoot") and loaded:
+	if Input.is_action_just_pressed("shoot") and loaded and reloaded:
 		
 		if(mouseIcon._isInAngle(CurrentDirection,self.global_position.direction_to(get_global_mouse_position()))):
 				loaded=false
 				timer.start()
 				_set_up_cannon_ball()
+				self.cannonBallsLeft -= 1
+				updateCannonBalls()
+				if self.cannonBallsLeft <= 0:
+					reloaded = false
+					reloadTimer.wait_time = self.stats["ReloadTime"]
+					reloadTimer.start()
 				
 	else:
 		mouseIcon._isInAngle(CurrentDirection,self.global_position.direction_to(get_global_mouse_position()))
@@ -111,6 +119,14 @@ func takeDamage(damage):
 	var deduction = damage * HeartSize
 	$Camera2D/HealthUI/HeartUIFull.rect_size.x -= deduction
 	
+func updateCannonBalls():
+	$Camera2D/CannonUI/Cannon.rect_size.x = self.cannonBallsLeft * CannonSize
+	if self.cannonBallsLeft <= 0:
+		$Camera2D/CannonUI/Cannon.visible = false
+	else:
+		$Camera2D/CannonUI/Cannon.visible = true
+
+	
 func increaseLevel():
 	self.level += 1
 	
@@ -120,7 +136,7 @@ func increaseHealth():
 	resetHearts()
 	
 func increaseDamage():
-	self.stats["DamageMultiplicator"] += 1
+	self.stats["DamageMultiplicator"] += 1 
 	
 func increaseCannonBalls():
 	self.stats["CannonBalls"] += 1
@@ -129,7 +145,7 @@ func increaseBoatSpeed():
 	self.stats["BoatSpeed"] += 1
 	
 func decreaseReloadTime():
-	self.stats["ReloadTime"] -= 1
+	self.stats["ReloadTime"] -= 1 #todo
 	
 func increaseRange():
 	self.stats["Range"] += 1
@@ -143,4 +159,9 @@ func increaseCoins():
 func _on_timer_timeout():
 	loaded=true
 	pass # Replace with function body.
+	
+func _on_ReloadTimer_timeout():
+	reloaded = true
+	self.cannonBallsLeft = 5
+	updateCannonBalls()
 
